18899614fb4638f0b57ee9fb016d762ea8bda398
filiperfigos <59123109+filiperfigos@users.noreply.github.com>
2019-12-26 17:16:44 -0300

    Update Simulacoes.R

--- Simulacoes.R
+++ Simulacoes.R
@@ -4,16 +4,16 @@ if(!require(metafor)){install.packages("metafor")}
 if(!require(psych)){install.packages("psych")}
 
 #####################
-##### Simulação #####
+##### SimulaÃ§Ã£o #####
 #####################
 
-K = c(3, 5, 10, 15, 20, 30, 40) #Número de estudos presentes na metanálise
-p = 0.01 #Prevalência escolhida
-sigma = 0.05 #Variação entre os estudos escolhida
-nsim = 1000 #Número de simulações
+K = c(3, 5, 10, 15, 20, 30, 40) #NÃºmero de estudos presentes na metanÃ¡lise
+p = 0.01 #PrevalÃªncia escolhida
+sigma = 0.05 #VariaÃ§Ã£o entre os estudos escolhida
+nsim = 1000 #NÃºmero de simulaÃ§Ãµes
 set.seed(220154)
 
-comparações = list()
+comparaÃ§Ãµes = list()
 
 for(k in 1:length(K)){
   
@@ -111,26 +111,26 @@ for(k in 1:length(K)){
     
   }
   
-  comparações[[k]] = aux
+  comparaÃ§Ãµes[[k]] = aux
   
 }
 
 for(k in 1:length(K)){
   
-  comparações[[k]] = data.frame(comparações[[k]])
+  comparaÃ§Ãµes[[k]] = data.frame(comparaÃ§Ãµes[[k]])
   
-  names(comparações[[k]]) = c("logito_aleat", "Li",  "Ls", "coverage", "Q", "I2", "tau", "arcoseno_aleat", "Li", "Ls", "coverage", "Q", "I2", "tau", "arcosenod_aleat harm", "Li", "Ls", "coverage", "arcosenod_aleat arit", "Li", "Ls", "coverage", "arcosenod_aleat geom", "Li", "Ls", "coverage", "arcosenod_aleat poolv", "Li", "Ls", "Q", "I2", "tau", "glmm_aleat", "Li", "Ls", "coverage", "Q", "I2", "tau")
+  names(comparaÃ§Ãµes[[k]]) = c("logito_aleat", "Li",  "Ls", "coverage", "Q", "I2", "tau", "arcoseno_aleat", "Li", "Ls", "coverage", "Q", "I2", "tau", "arcosenod_aleat harm", "Li", "Ls", "coverage", "arcosenod_aleat arit", "Li", "Ls", "coverage", "arcosenod_aleat geom", "Li", "Ls", "coverage", "arcosenod_aleat poolv", "Li", "Ls", "Q", "I2", "tau", "glmm_aleat", "Li", "Ls", "coverage", "Q", "I2", "tau")
   
-  comparações[[k]][, 1] =  transf.ilogit(comparações[[k]][, 1])
-  comparações[[k]][, 2] =  transf.ilogit(comparações[[k]][, 2])
-  comparações[[k]][, 3] =  transf.ilogit(comparações[[k]][, 3])
-  comparações[[k]][, 34] =  transf.ilogit(comparações[[k]][, 34])
-  comparações[[k]][, 35] =  transf.ilogit(comparações[[k]][, 35])
-  comparações[[k]][, 36] =  transf.ilogit(comparações[[k]][, 36])
+  comparaÃ§Ãµes[[k]][, 1] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 1])
+  comparaÃ§Ãµes[[k]][, 2] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 2])
+  comparaÃ§Ãµes[[k]][, 3] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 3])
+  comparaÃ§Ãµes[[k]][, 34] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 34])
+  comparaÃ§Ãµes[[k]][, 35] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 35])
+  comparaÃ§Ãµes[[k]][, 36] =  transf.ilogit(comparaÃ§Ãµes[[k]][, 36])
   
-  comparações[[k]][, 8] =  transf.iarcsin(comparações[[k]][, 8])
-  comparações[[k]][, 9] =  transf.iarcsin(comparações[[k]][, 9])
-  comparações[[k]][, 10] =  transf.iarcsin(comparações[[k]][, 10])
+  comparaÃ§Ãµes[[k]][, 8] =  transf.iarcsin(comparaÃ§Ãµes[[k]][, 8])
+  comparaÃ§Ãµes[[k]][, 9] =  transf.iarcsin(comparaÃ§Ãµes[[k]][, 9])
+  comparaÃ§Ãµes[[k]][, 10] =  transf.iarcsin(comparaÃ§Ãµes[[k]][, 10])
   
 }
 
@@ -138,19 +138,19 @@ resultados1 = list()
 
 for(k in 1:length(K)){
   
-  aux = data.frame(rbind(c(apply(comparações[[k]][, c(1:3)], 2, mean, na.rm = T), sum(comparações[[k]][, 4], na.rm = T), sum(is.na(comparações[[k]][, 1]) == "TRUE"), sum((p-comparações[[k]][, 1])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(5:7)], 2, mean, na.rm = T)),
+  aux = data.frame(rbind(c(apply(comparaÃ§Ãµes[[k]][, c(1:3)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 4], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 1]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 1])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(5:7)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(8:10)], 2, mean, na.rm = T), sum(comparações[[k]][, 11], na.rm = T), sum(is.na(comparações[[k]][, 8]) == "TRUE"), sum((p-comparações[[k]][, 8])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(12:14)], 2, mean, na.rm = T)),
+                         c(apply(comparaÃ§Ãµes[[k]][, c(8:10)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 11], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 8]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 8])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(12:14)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(15:17)], 2, mean, na.rm = T), sum(comparações[[k]][, 18], na.rm = T), sum(is.na(comparações[[k]][, 15]) == "TRUE"), sum((p-comparações[[k]][, 15])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(31:33)], 2, mean, na.rm = T)),
+                         c(apply(comparaÃ§Ãµes[[k]][, c(15:17)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 18], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 15]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 15])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(31:33)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(19:21)], 2, mean, na.rm = T), sum(comparações[[k]][, 22], na.rm = T), sum(is.na(comparações[[k]][, 19]) == "TRUE"), sum((p-comparações[[k]][, 19])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(31:33)], 2, mean, na.rm = T)),
+                         c(apply(comparaÃ§Ãµes[[k]][, c(19:21)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 22], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 19]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 19])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(31:33)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(23:25)], 2, mean, na.rm = T), sum(comparações[[k]][, 26], na.rm = T), sum(is.na(comparações[[k]][, 23]) == "TRUE"), sum((p-comparações[[k]][, 24])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(31:33)], 2, mean, na.rm = T)),
+                         c(apply(comparaÃ§Ãµes[[k]][, c(23:25)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 26], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 23]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 24])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(31:33)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(27:29)], 2, mean, na.rm = T), sum(comparações[[k]][, 30], na.rm = T), sum(is.na(comparações[[k]][, 27]) == "TRUE"), sum((p-comparações[[k]][, 27])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(31:33)], 2, mean, na.rm = T)),
+                         c(apply(comparaÃ§Ãµes[[k]][, c(27:29)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 30], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 27]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 27])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(31:33)], 2, mean, na.rm = T)),
                          
-                         c(apply(comparações[[k]][, c(34:36)], 2, mean, na.rm = T), sum(comparações[[k]][, 37], na.rm = T), sum(is.na(comparações[[k]][, 34]) == "TRUE"), sum((p-comparações[[k]][, 34])^2, na.rm = T)/nsim, apply(comparações[[k]][, c(38:40)], 2, mean, na.rm = T))))
+                         c(apply(comparaÃ§Ãµes[[k]][, c(34:36)], 2, mean, na.rm = T), sum(comparaÃ§Ãµes[[k]][, 37], na.rm = T), sum(is.na(comparaÃ§Ãµes[[k]][, 34]) == "TRUE"), sum((p-comparaÃ§Ãµes[[k]][, 34])^2, na.rm = T)/nsim, apply(comparaÃ§Ãµes[[k]][, c(38:40)], 2, mean, na.rm = T))))
   
   names(aux) = c("me", "li", "ls", "coverage", "countna", "erro_quadr", "Q", "I2", "tau")
   aux$coverage = aux$coverage/nsim
@@ -166,7 +166,8 @@ for(k in 1:length(K)){
   
 }
 
-print(resultados1)
+print(comparaÃ§Ãµes) #Lista com as 7 matrizes com 1000 linhas cada
+print(resultados1) #Medidas jÃ¡ prontas para anÃ¡lise
 
 ###################
 ##### Figuras #####
@@ -175,24 +176,24 @@ print(resultados1)
 if(!require(ggplot2)){install.packages("ggplot2")}
 if(!require(gridExtra)){install.packages("gridExtra")}
 
-dados1 = subset(resultados1, ) #Dados que serão utilizados para o primeiro gráfico da figura
-dados2 = subset(resultados1, ) #Dados que serão utilizados para o segundo gráfico da figura
-dados3 = subset(resultados1, ) #Dados que serão utilizados para o terceiro gráfico da figura
-dados4 = subset(resultados1, ) #Dados que serão utilizados para o quarto gráfico da figura
+dados1 = subset(resultados1, ) #Dados que serÃ£o utilizados para o primeiro grÃ¡fico da figura
+dados2 = subset(resultados1, ) #Dados que serÃ£o utilizados para o segundo grÃ¡fico da figura
+dados3 = subset(resultados1, ) #Dados que serÃ£o utilizados para o terceiro grÃ¡fico da figura
+dados4 = subset(resultados1, ) #Dados que serÃ£o utilizados para o quarto grÃ¡fico da figura
 
-#Calculando o limite mínimo e o máximo da variável que será plotada no eixo Y
+#Calculando o limite mÃ­nimo e o mÃ¡ximo da variÃ¡vel que serÃ¡ plotada no eixo Y
 minimo = min(dados1$variavel, dados2$variavel, dados3$variavel, dados4$variavel)
 maximo = max(dados1$variavel, dados2$variavel, dados3$variavel, dados4$variavel)
 
-p1 = ggplot(dados1, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "Título do gráfico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) 
+p1 = ggplot(dados1, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "TÃ­tulo do grÃ¡fico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) 
 
-p2 = ggplot(dados2, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "Título do gráfico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
+p2 = ggplot(dados2, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "TÃ­tulo do grÃ¡fico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
 
-p3 = ggplot(dados3, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "Título do gráfico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
+p3 = ggplot(dados3, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "TÃ­tulo do grÃ¡fico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
 
-p4 = ggplot(dados4, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "Título do gráfico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
+p4 = ggplot(dados4, aes(x = K, y = variavel, group = forma_de_estimacao))  + geom_line(aes(color = forma_de_estimacao), alpha = 0.5, size = 1) + geom_point(aes(shape = forma_de_estimacao, color = forma_de_estimacao), alpha = 0.75, size = 4) + scale_x_continuous(breaks = c(3, 5, 10, 15, 20, 30, 40)) + theme_bw() + labs(title = "TÃ­tulo do grÃ¡fico") + theme(plot.title = element_text(hjust = 0.5)) + geom_hline(yintercept = 0, linetype = "dashed", color = "red") + ylim(c(minimo, maximo)) + theme(axis.title.y = element_blank()) + theme(axis.title.x = element_blank()) + theme(legend.position='none')
 
 legend = get_legend(p1)
 p1 = p1 + theme(legend.position='none')
 
-grid.arrange(p1, p2, p3, p4, legend, nrow = 2, ncol = 3, left = text_grob("Variável", rot = 90, vjust = 1), bottom = text_grob("Número de estudos", hjust = 1.3), layout_matrix = rbind(c(1,1,2,2,5), c(3,3,4,4,5)))
+grid.arrange(p1, p2, p3, p4, legend, nrow = 2, ncol = 3, left = text_grob("VariÃ¡vel", rot = 90, vjust = 1), bottom = text_grob("NÃºmero de estudos", hjust = 1.3), layout_matrix = rbind(c(1,1,2,2,5), c(3,3,4,4,5)))
